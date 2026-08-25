9dda2cf6572ebee7e9ad233a53cb370baa46cb0a
Konstantin Tarmyshov <67151862+ktarmyshov@users.noreply.github.com>
2026-08-25 12:58:37 +0200

    coverage to v8 (#125)

---
+++ .changeset/coverage-v8.md
@@ -0,0 +1,5 @@
+---
+'@mirasen/chessboard': patch
+---
+
+chore: switch coverage provider from istanbul to v8
--- vitest.config.ts
+++ vitest.config.ts
@@ -6,8 +6,7 @@ export default defineConfig({
 		include: ['tests/**/*.spec.ts'],
 		coverage: {
 			reportsDirectory: './coverage-test',
-			provider: 'istanbul', // or 'v8'
-			// provider: 'v8',
+			provider: 'v8',
 			include: ['src/**/*.{js,ts}'],
 			reporter: ['text', 'html', 'clover', 'json', 'lcov']
 		}
